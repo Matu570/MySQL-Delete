020c56e41dbf7500bf116bc161c94f34faf1b2b7
Matias Bueno <matiasbueno@hotmail.com.ar>
2022-11-28 17:49:46 -0300

    Delete

---
+++ Update-and-Delete.sql
@@ -0,0 +1,7 @@
+/*
+Update datas:
+UPDATE users SET date = CURDATE() WHERE id_user=4;
+UPDATE users SET date = '2022-05-21' WHERE id_user=4;
+
+Delete records:
+DELETE FROM users WHERE email = 'email@email';
